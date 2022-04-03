b8bf601b903902ca178e49fecd5817cf70c4c2fb
Aliaksandr Pauliukavets <al.pavlyukovets@yandex.ru>
2022-04-03 18:43:07 +0300

    feat: solve simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,32 +1,44 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    chain: [],
+    getLength() {
+        return this.chain.length;
+        // remove line with error and write your code here
+    },
+    addLink(value) {
+        this.chain.push(`( ${String(value)} )`);
+        return this;
+        // remove line with error and write your code here
+    },
+    removeLink(position) {
+        if (typeof position === 'number' && position > 0 && position < this.chain.length) {
+            this.chain.splice(position - 1, 1);
+            return this;
+        } else {
+            this.chain = [];
+            throw new NotImplementedError('You can\'t remove incorrect link!');
+        }
+
+        // remove line with error and write your code here
+    },
+    reverseChain() {
+        this.chain.reverse();
+        return this;
+        // remove line with error and write your code here
+    },
+    finishChain() {
+        let resultChain = this.chain.join('~~');
+        this.chain = [];
+        return resultChain;
+        // remove line with error and write your code here
+    }
 };
 
 module.exports = {
-  chainMaker
+    chainMaker
 };
